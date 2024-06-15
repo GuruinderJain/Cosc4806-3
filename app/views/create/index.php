59221771d637ce43b3ba3256d704a84ecd132c61
GuruinderJain <gujain@algomau.ca>
2024-06-15 00:31:20 +0000

    add account

--- app/views/create/index.php
+++ app/views/create/index.php
@@ -24,13 +24,30 @@
                 <label for="password">Confirm Password</label>
                 <input required type="password" class="form-control" name="password2">
               </div>
-                <br>
+            <?php
+            if (isset($_SESSION['username_exists']) && $_SESSION['username_exists'] == true) {
+                echo "Username already taken";
+            }
+            else if (isset($_SESSION['password_mismatch']) && $_SESSION['password_mismatch'] == 1) {
+                echo "Passwords do not match";
+              }
+              else if (isset($_SESSION['password_too_short']) && $_SESSION['password_too_short'] == 1) {
+                echo "Password must be at least 8 characters";
+              }
+
+            
+            unset($_SESSION['username_exists']);
+            unset($_SESSION['password_mismatch']);
+            unset($_SESSION['password_too_short']);
+            ?>
+            <br>
             <button type="submit" class="btn btn-primary">Sign up</button>
         </fieldset>
         </form> 
   </div>
 </div>
 <footer>
+    <a href="/login">Already have an account? Log in here.</a>
     <?php
     if (isset($_SESSION['username_exists']) && $_SESSION['username_exists'] == true) {
         echo "Username already taken";
@@ -42,6 +59,7 @@
         echo "Password must be at least 8 characters";
       }
 
+   
     unset($_SESSION['username_exists']);
     unset($_SESSION['password_mismatch']);
     unset($_SESSION['password_too_short']);
