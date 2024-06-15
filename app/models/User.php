c2f56f7f033ed9fafa6c12ee76ad58b5c667ad4e
GuruinderJain <gujain@algomau.ca>
2024-06-15 00:17:18 +0000

    password checking and requirements while adding account

--- app/models/User.php
+++ app/models/User.php
@@ -2,51 +2,50 @@
 
 class User {
 
-    public $username;
-    public $password;
-    public $auth = false;
+  public $username;
+  public $password;
+  public $auth = false;
 
-    public function __construct() {
-        
-    }
+  public function __construct() {
 
-    public function test () {
-      $db = db_connect();
-      $statement = $db->prepare("select * from users;");
-      $statement->execute();
-      $rows = $statement->fetch(PDO::FETCH_ASSOC);
-      return $rows;
-    }
+  }
 
-    public function authenticate($username, $password) {
-        /*
-         * if username and password good then
-         * $this->auth = true;
-         */
-		$username = strtolower($username);
-		$db = db_connect();
-        $statement = $db->prepare("select * from users WHERE username = :username;");
-        $statement->bindValue(':username', $username);
+  public function test () {
+    $db = db_connect();
+    $statement = $db->prepare("select * from users;");
+    $statement->execute();
+    $rows = $statement->fetch(PDO::FETCH_ASSOC);
+    return $rows;
+  }
+
+  public function authenticate($username, $password) {
+     
+    $username = strtolower($username);
+    $db = db_connect();
+        $statement = $db->prepare("select * from users WHERE username = :name;");
+        $statement->bindValue(':name', $username);
         $statement->execute();
         $rows = $statement->fetch(PDO::FETCH_ASSOC);
-		
-		if (password_verify($password, $rows['password'])) {
-			$_SESSION['auth'] = 1;
-			$_SESSION['username'] = ucwords($username);
-			unset($_SESSION['failedAuth']);
-			header('Location: /home');
-			die;
-		} else {
-			if(isset($_SESSION['failedAuth'])) {
-				$_SESSION['failedAuth'] ++; //increment
-			} else {
-				$_SESSION['failedAuth'] = 1;
-			}
-			header('Location: /login');
-			die;
-		}
+
+    if (password_verify($password, $rows['password'])) {
+      $_SESSION['auth'] = 1;
+      $_SESSION['username'] = ucwords($username);
+      unset($_SESSION['failedAuth']);
+      header('Location: /home');
+      die;
+    } else {
+      if(isset($_SESSION['failedAuth'])) {
+        $_SESSION['failedAuth'] ++;
+      } else {
+        $_SESSION['failedAuth'] = 1;
+      }
+      header('Location: /login'); 
+      die;
     }
-  public function checkUsernameExists($username) {
+  }
+
+ 
+  public function check_username_exists($username) {
     $_SESSION['test'] = 'test';
     $db = db_connect();
     $statement = $db->prepare("SELECT username FROM users WHERE username = '$username'");
@@ -55,10 +54,10 @@ class User {
 
 
     if (isset($row) && !empty($row)) {
-      $_SESSION['usernameExists'] = true;
+      $_SESSION['username_exists'] = true;
     }
     else {
-      $_SESSION['usernameExists'] = false;
+      $_SESSION['username_exists'] = false;
     }
     // die;
   }
